db5a7436d6760b62b6bdd63af84e59cea5f0995a
Quimisagi <m5281007@u-aizu.ac.jp>
2024-11-15 22:50:23 +0900

    Bottom navigation bar inproved

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,46 +1,65 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import { Tabs } from 'expo-router';
 import { FontAwesome6, Ionicons } from '@expo/vector-icons';
-import { View, TouchableOpacity, StyleSheet } from 'react-native';
+import { TouchableOpacity, StyleSheet } from 'react-native';
 import { router } from 'expo-router';
-import { Entypo } from '@expo/vector-icons';
 
 export default () => {
-  
   return (
     <Tabs
       screenOptions={{
-          showLabel: false,
-        }}
+        showLabel: false,
+      }}
     >
       <Tabs.Screen 
-      name="home" 
-      options={{
-        title: '',
-        tabBarIcon: (focused) => (
-          <FontAwesome6 style={styles.button} name="house" size={24} color={focused ? '#000' : '#89A1A9'} />
-        ),
-      }}
+        name="home" 
+        options={{
+          title: '',
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome6
+              style={styles.button}
+              name="house"
+              size={24}
+              color={focused ? '#333333' : '#89A1A9'}
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name="transactionsPlaceholder"
         options={{
           title: '',
-          tabBarButton: (focused) => (
+          tabBarButton: ({ focused }) => (
             <TouchableOpacity
               style={{
                 alignItems: 'center',
                 justifyContent: 'center',
-                backgroundColor: focused ? '#00A5E0' : '#FFF',
-                borderRadius: 50,
                 top: -25,
                 width: 65,
                 height: 65,
               }}
               onPress={() => {
-                router.push({ pathname: '/transactionsForm'});
+                router.push({ pathname: '/transactionsForm' });
               }}
             >
-              <Ionicons name="add-circle-outline" size={50} color={focused ? '#FFF' : '#000'} />
+              <LinearGradient
+                colors={['#BBE0F2', '#00A5E0']} // Gradient colors
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 1 }}
+                style={{
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  borderRadius: 50,
+                  width: 65,
+                  height: 65,
+                }}
+              >
+                <Ionicons
+                  name="add-circle-outline"
+                  size={50}
+                  color={focused ? '#FFF' : '#000'}
+                />
+              </LinearGradient>
             </TouchableOpacity>
           ),
         }}
@@ -49,17 +68,22 @@ export default () => {
         name="accountsList" 
         options={{
           title: '',
-          tabBarIcon: (focused) => (
-            <FontAwesome6 style={styles.button} name="wallet" size={24} color={focused ? '#000' : '#000'} />
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome6
+              style={styles.button}
+              name="wallet"
+              size={24}
+              color={focused ? '#333333' : '#89A1A9'}
+            />
           ),
-        }}/>
+        }}
+      />
     </Tabs>
   );
-}
+};
 
 const styles = StyleSheet.create({
   button: {
     marginTop: 10,
   },
 });
-
